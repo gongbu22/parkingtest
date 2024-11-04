4ce27d69882e01db67e5912a8a6fbfc08232b6c5
gongbu22 <pyujin0711@naver.com>
2024-11-04 13:44:32 +0900

    model에 Parking테이블에 carnum 컬럼에 unique=True 추가

--- msa-parking-service/models/parking.py
+++ msa-parking-service/models/parking.py
@@ -10,7 +10,7 @@ class Parking(Base):
     __tablename__ = 'parking'
 
     pno = Column(Integer, primary_key=True, autoincrement=True, index=True)
-    carnum = Column(String(10), nullable=False)
+    carnum = Column(String(10), nullable=False, unique=True)
     barrier = Column(String(5), nullable=False, default='0')
     intime = Column(DateTime, default=datetime.now)
     outtime = Column(DateTime, nullable=True)
